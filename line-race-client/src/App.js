3ac078ebaf43d84c632dfd7cee09b43b2a0c3d46
eugene.malin <jenia96@live.com>
2022-03-02 01:55:03 +0300

    Add hello world app

---
+++ line-race-client/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Simple paper line race game
+        </p>
+        <a href="https://www.flaticon.com/free-icons/race" title="race icons">Race icons created by Freepik - Flaticon</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
